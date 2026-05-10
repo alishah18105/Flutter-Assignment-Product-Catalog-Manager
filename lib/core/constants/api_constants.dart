e2306286d2b5cf054badc7d4cd275684af756999
alishah18105 <alishah18105@gmail.com>
2026-05-10 13:54:39 +0500

    Api_Constants.dart file updated

---
+++ lib/core/constants/api_constants.dart
@@ -0,0 +1,4 @@
+class ApiConstants{
+  static const String baseUrl = "https://crudcrud.com/api/179501f94ed1405b9ea637b581a37395";
+  static const String productsEndpoint = "${baseUrl}/newproducts";
+}
